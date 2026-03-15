77ae431e70f05f6fe4d743e761a1a6378067a0ba
ewrnahid <nahidul2.000001@gmail.com>
2026-03-15 23:52:29 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 particlesJS("particles-js", {
-particles: {
-number: { value: 80 },
-color: { value: "#FFD700" },
-shape: { type: "circle" },
-opacity: { value: 0.5 },
-size: { value: 3 },
-move: { speed: 2 }
-}
+  particles: {
+    number: { value: 80 },
+    color: { value: "#FFD700" },
+    shape: { type: "circle" },
+    opacity: { value: 0.5 },
+    size: { value: 3 },
+    move: { speed: 2 }
+  }
 });
 
-function startBot(){
-document.getElementById("botStatus").innerText="Status: ON";
+function startBot() {
+  document.getElementById("botStatus").innerText = "Status: ON";
 }
 
-function stopBot(){
-document.getElementById("botStatus").innerText="Status: OFF";
+function stopBot() {
+  document.getElementById("botStatus").innerText = "Status: OFF";
 }
